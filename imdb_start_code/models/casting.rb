daf001c21b8170f4220cbfbd17ffde2cfcb682b6
Fiona Wilson <fionaw237@gmail.com>
2018-08-02 14:35:11 +0100

    Adds save and delete_all methods

--- imdb_start_code/models/casting.rb
+++ imdb_start_code/models/casting.rb
@@ -1,3 +1,28 @@
+require_relative("../db/sql_runner")
+
 class Casting
+
+  attr_reader :id, :movie_id, :star_id
+  attr_accessor :fee
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @movie_id = options['movie_id']
+    @star_id = options['star_id']
+    @fee = options['fee']
+  end
+
+  def save()
+    sql = "INSERT INTO castings (movie_id, star_id, fee)
+           VALUES ($1, $2, $3) RETURNING id"
+    values = [@movie_id, @star_id, @fee]
+    casting = SqlRunner.run(sql, values).first()
+    @id = casting['id'].to_i()
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM castings"
+    SqlRunner.run(sql)
+  end
 
 end
